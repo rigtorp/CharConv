38e8a5ac75c541d96457128aa5af12455f505aa1
Erik Rigtorp <erik@rigtorp.se>
2018-09-16 19:39:11 -0500

    Add unsigned and 64bit integer benchmarks

--- src/CharConvBenchmark.cpp
+++ src/CharConvBenchmark.cpp
@@ -29,40 +29,44 @@ SOFTWARE.
 #include <rigtorp/CharConv.h>
 #include <sstream>
 
-auto generate_ints(int digits) {
-  if (digits > 9) {
-    std::abort();
-  }
-  int max = 1;
+template <typename T> auto generate_ints(int digits) {
+  T max = 1;
   for (int i = 0; i < digits; ++i) {
     max *= 10;
   }
   --max;
+  const T min = std::is_unsigned_v<T> ? 0 : -max;
   std::random_device rd;
   std::mt19937 gen(rd());
-  std::uniform_int_distribution<int> dis(-max, max);
-  std::array<int, 4096> v = {};
-  std::generate(v.begin(), v.end(), [&] { return dis(gen); });
+  std::uniform_int_distribution<T> dis(min, max);
+  std::array<T, 4096> v = {};
+  std::generate(v.begin(), v.end(), [&] {
+    T res;
+    do {
+      res = dis(gen);
+    } while (std::to_string(res).size() < static_cast<size_t>(digits));
+    return res;
+  });
   return v;
 }
 
-auto generate_strings(int digits) {
-  if (digits > 9) {
-    std::abort();
-  }
-  int max = 1;
+template <typename T> auto generate_strings(int digits) {
+  T max = 1;
   for (int i = 0; i < digits; ++i) {
     max *= 10;
   }
   --max;
+  const T min = std::is_unsigned_v<T> ? 0 : -max;
   std::random_device rd;
   std::mt19937 gen(rd());
-  std::uniform_int_distribution<int> dis(-max, max);
+  std::uniform_int_distribution<T> dis(min, max);
   std::array<std::string, 4096> v;
   std::generate(v.begin(), v.end(), [&] {
-    std::array<char, 32> b = {};
-    sprintf(b.data(), "%i", dis(gen));
-    return b.data();
+    std::string res;
+    do {
+      res = std::to_string(dis(gen));
+    } while (res.size() < static_cast<size_t>(digits));
+    return res;
   });
   return v;
 }
@@ -94,7 +98,7 @@ bool from_chars_unchecked(const char *first, const char *last,
 }
 
 static void BM_sprintf(benchmark::State &state) {
-  const auto v = generate_ints(state.range(0));
+  const auto v = generate_ints<int>(state.range(0));
   std::array<char, 16> buf = {};
   int i = 0;
   for (auto _ : state) {
@@ -104,7 +108,7 @@ static void BM_sprintf(benchmark::State &state) {
 }
 
 static void BM_to_string(benchmark::State &state) {
-  const auto v = generate_ints(state.range(0));
+  const auto v = generate_ints<int>(state.range(0));
   std::string buf;
   int i = 0;
   for (auto _ : state) {
@@ -114,7 +118,7 @@ static void BM_to_string(benchmark::State &state) {
 }
 
 static void BM_stringstream(benchmark::State &state) {
-  const auto v = generate_ints(state.range(0));
+  const auto v = generate_ints<int>(state.range(0));
   std::stringstream ss;
   int i = 0;
   for (auto _ : state) {
@@ -123,9 +127,9 @@ static void BM_stringstream(benchmark::State &state) {
   }
 }
 
-static void BM_std_to_chars(benchmark::State &state) {
-  const auto v = generate_ints(state.range(0));
-  std::array<char, 16> buf = {};
+template <typename T> static void BM_std_to_chars(benchmark::State &state) {
+  const auto v = generate_ints<T>(state.range(0));
+  std::array<char, 32> buf = {};
   int i = 0;
   for (auto _ : state) {
     benchmark::DoNotOptimize(buf.data());
@@ -137,7 +141,7 @@ static void BM_std_to_chars(benchmark::State &state) {
 }
 
 static void BM_rigtorp_to_chars_naive(benchmark::State &state) {
-  const auto v = generate_ints(state.range(0));
+  const auto v = generate_ints<int>(state.range(0));
   std::array<char, 16> buf = {};
   int i = 0;
   for (auto _ : state) {
@@ -149,9 +153,9 @@ static void BM_rigtorp_to_chars_naive(benchmark::State &state) {
   }
 }
 
-static void BM_rigtorp_to_chars(benchmark::State &state) {
-  const auto v = generate_ints(state.range(0));
-  std::array<char, 16> buf = {};
+template <typename T> static void BM_rigtorp_to_chars(benchmark::State &state) {
+  const auto v = generate_ints<T>(state.range(0));
+  std::array<char, 32> buf = {};
   int i = 0;
   for (auto _ : state) {
     benchmark::DoNotOptimize(buf.data());
@@ -163,7 +167,7 @@ static void BM_rigtorp_to_chars(benchmark::State &state) {
 }
 
 static void BM_atoi(benchmark::State &state) {
-  auto v = generate_strings(state.range(0));
+  const auto v = generate_strings<int>(state.range(0));
   int i = 0;
   for (auto _ : state) {
     benchmark::DoNotOptimize(atoi(v[i % v.size()].data()));
@@ -172,7 +176,7 @@ static void BM_atoi(benchmark::State &state) {
 }
 
 static void BM_strtol(benchmark::State &state) {
-  auto v = generate_strings(state.range(0));
+  const auto v = generate_strings<long>(state.range(0));
   int i = 0;
   for (auto _ : state) {
     benchmark::DoNotOptimize(strtol(v[i % v.size()].data(), nullptr, 10));
@@ -181,7 +185,7 @@ static void BM_strtol(benchmark::State &state) {
 }
 
 static void BM_stoi(benchmark::State &state) {
-  auto v = generate_strings(state.range(0));
+  const auto v = generate_strings<int>(state.range(0));
   int i = 0;
   for (auto _ : state) {
     benchmark::DoNotOptimize(std::stoi(v[i % v.size()]));
@@ -189,9 +193,9 @@ static void BM_stoi(benchmark::State &state) {
   }
 }
 
-static void BM_std_from_chars(benchmark::State &state) {
-  auto v = generate_strings(state.range(0));
-  int val = 0;
+template <typename T> static void BM_std_from_chars(benchmark::State &state) {
+  const auto v = generate_strings<T>(state.range(0));
+  T val = 0;
   int i = 0;
   for (auto _ : state) {
     const auto &s = v[i % v.size()];
@@ -203,7 +207,7 @@ static void BM_std_from_chars(benchmark::State &state) {
 }
 
 static void BM_rigtorp_from_chars_unchecked(benchmark::State &state) {
-  auto v = generate_strings(state.range(0));
+  const auto v = generate_strings<uint32_t>(state.range(0));
   uint32_t val = 0;
   int i = 0;
   for (auto _ : state) {
@@ -215,9 +219,10 @@ static void BM_rigtorp_from_chars_unchecked(benchmark::State &state) {
   }
 }
 
+template <typename T>
 static void BM_rigtorp_from_chars(benchmark::State &state) {
-  auto v = generate_strings(state.range(0));
-  int val = 0;
+  const auto v = generate_strings<T>(state.range(0));
+  T val = 0;
   int i = 0;
   for (auto _ : state) {
     const auto &s = v[i % v.size()];
@@ -228,136 +233,35 @@ static void BM_rigtorp_from_chars(benchmark::State &state) {
   }
 }
 
-BENCHMARK(BM_sprintf)
-    ->Arg(1)
-    ->Arg(2)
-    ->Arg(3)
-    ->Arg(4)
-    ->Arg(5)
-    ->Arg(6)
-    ->Arg(7)
-    ->Arg(8)
-    ->Arg(9);
+template <int N> static void Digits(benchmark::internal::Benchmark *b) {
+  for (int i = 1; i <= N; ++i) {
+    b->Arg(i);
+  }
+}
 
-BENCHMARK(BM_to_string)
-    ->Arg(1)
-    ->Arg(2)
-    ->Arg(3)
-    ->Arg(4)
-    ->Arg(5)
-    ->Arg(6)
-    ->Arg(7)
-    ->Arg(8)
-    ->Arg(9);
-
-BENCHMARK(BM_stringstream)
-    ->Arg(1)
-    ->Arg(2)
-    ->Arg(3)
-    ->Arg(4)
-    ->Arg(5)
-    ->Arg(6)
-    ->Arg(7)
-    ->Arg(8)
-    ->Arg(9);
-
-BENCHMARK(BM_std_to_chars)
-    ->Arg(1)
-    ->Arg(2)
-    ->Arg(3)
-    ->Arg(4)
-    ->Arg(5)
-    ->Arg(6)
-    ->Arg(7)
-    ->Arg(8)
-    ->Arg(9);
-
-BENCHMARK(BM_rigtorp_to_chars_naive)
-    ->Arg(1)
-    ->Arg(2)
-    ->Arg(3)
-    ->Arg(4)
-    ->Arg(5)
-    ->Arg(6)
-    ->Arg(7)
-    ->Arg(8)
-    ->Arg(9);
-
-BENCHMARK(BM_rigtorp_to_chars)
-    ->Arg(1)
-    ->Arg(2)
-    ->Arg(3)
-    ->Arg(4)
-    ->Arg(5)
-    ->Arg(6)
-    ->Arg(7)
-    ->Arg(8)
-    ->Arg(9);
-
-BENCHMARK(BM_atoi)
-    ->Arg(1)
-    ->Arg(2)
-    ->Arg(3)
-    ->Arg(4)
-    ->Arg(5)
-    ->Arg(6)
-    ->Arg(7)
-    ->Arg(8)
-    ->Arg(9);
-
-BENCHMARK(BM_strtol)
-    ->Arg(1)
-    ->Arg(2)
-    ->Arg(3)
-    ->Arg(4)
-    ->Arg(5)
-    ->Arg(6)
-    ->Arg(7)
-    ->Arg(8)
-    ->Arg(9);
-
-BENCHMARK(BM_stoi)
-    ->Arg(1)
-    ->Arg(2)
-    ->Arg(3)
-    ->Arg(4)
-    ->Arg(5)
-    ->Arg(6)
-    ->Arg(7)
-    ->Arg(8)
-    ->Arg(9);
-
-BENCHMARK(BM_std_from_chars)
-    ->Arg(1)
-    ->Arg(2)
-    ->Arg(3)
-    ->Arg(4)
-    ->Arg(5)
-    ->Arg(6)
-    ->Arg(7)
-    ->Arg(8)
-    ->Arg(9);
-
-BENCHMARK(BM_rigtorp_from_chars_unchecked)
-    ->Arg(1)
-    ->Arg(2)
-    ->Arg(3)
-    ->Arg(4)
-    ->Arg(5)
-    ->Arg(6)
-    ->Arg(7)
-    ->Arg(8)
-    ->Arg(9);
-
-BENCHMARK(BM_rigtorp_from_chars)
-    ->Arg(1)
-    ->Arg(2)
-    ->Arg(3)
-    ->Arg(4)
-    ->Arg(5)
-    ->Arg(6)
-    ->Arg(7)
-    ->Arg(8)
-    ->Arg(9);
+BENCHMARK(BM_sprintf)->Apply(Digits<9>);
+BENCHMARK(BM_to_string)->Apply(Digits<9>);
+BENCHMARK(BM_stringstream)->Apply(Digits<9>);
+BENCHMARK_TEMPLATE(BM_std_to_chars, int32_t)->Apply(Digits<9>);
+BENCHMARK_TEMPLATE(BM_std_to_chars, uint32_t)->Apply(Digits<9>);
+BENCHMARK_TEMPLATE(BM_std_to_chars, int64_t)->Apply(Digits<19>);
+BENCHMARK_TEMPLATE(BM_std_to_chars, uint64_t)->Apply(Digits<19>);
+BENCHMARK(BM_rigtorp_to_chars_naive)->Apply(Digits<9>);
+BENCHMARK_TEMPLATE(BM_rigtorp_to_chars, int32_t)->Apply(Digits<9>);
+BENCHMARK_TEMPLATE(BM_rigtorp_to_chars, uint32_t)->Apply(Digits<9>);
+BENCHMARK_TEMPLATE(BM_rigtorp_to_chars, int64_t)->Apply(Digits<19>);
+BENCHMARK_TEMPLATE(BM_rigtorp_to_chars, uint64_t)->Apply(Digits<19>);
+BENCHMARK(BM_atoi)->Apply(Digits<9>);
+BENCHMARK(BM_strtol)->Apply(Digits<9>);
+BENCHMARK(BM_stoi)->Apply(Digits<9>);
+BENCHMARK_TEMPLATE(BM_std_from_chars, int32_t)->Apply(Digits<9>);
+BENCHMARK_TEMPLATE(BM_std_from_chars, uint32_t)->Apply(Digits<9>);
+BENCHMARK_TEMPLATE(BM_std_from_chars, int64_t)->Apply(Digits<19>);
+BENCHMARK_TEMPLATE(BM_std_from_chars, uint64_t)->Apply(Digits<19>);
+BENCHMARK(BM_rigtorp_from_chars_unchecked)->Apply(Digits<9>);
+BENCHMARK_TEMPLATE(BM_rigtorp_from_chars, int32_t)->Apply(Digits<9>);
+BENCHMARK_TEMPLATE(BM_rigtorp_from_chars, uint32_t)->Apply(Digits<9>);
+BENCHMARK_TEMPLATE(BM_rigtorp_from_chars, int64_t)->Apply(Digits<19>);
+BENCHMARK_TEMPLATE(BM_rigtorp_from_chars, uint64_t)->Apply(Digits<19>);
 
 BENCHMARK_MAIN();
